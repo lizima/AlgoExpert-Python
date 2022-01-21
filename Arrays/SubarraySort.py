b192840532b72ce5752ec39f74414c6086ba4568
Wenye Ma <mwysjtu@gmail.com>
2022-01-20 21:30:05 -0500

    0120

---
+++ Arrays/SubarraySort.py
@@ -0,0 +1,27 @@
+def subarraySort(array):
+    minOutofOrder = float("inf")
+	maxOutofOrder = float("-inf")
+	for i in range(len(array)):
+		num = array[i]
+		if isOutofOrder(i,num,array):
+			minOutofOrder = min(minOutofOrder,num)
+			maxOutofOrder = max(maxOutofOrder,num)
+	
+	if minOutofOrder == float("inf"):
+		return [-1,-1]
+	
+	subarrayLeftIdx = 0
+	while minOutofOrder >= array[subarrayLeftIdx]:
+		subarrayLeftIdx += 1
+		
+	subarrayRightIdx = len(array) - 1
+	while maxOutofOrder <= array[subarrayRightIdx]:
+		subarrayRightIdx -= 1
+	return [subarrayLeftIdx,subarrayRightIdx]
+	
+def isOutofOrder(i,num,array):
+	if i == 0:
+		return num > array[i+1]
+	if i == len(array) - 1:
+		return num < array[i-1]
+	return num > array[i+1] or num < array[i-1]
